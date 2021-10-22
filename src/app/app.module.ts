a62736374af59bc6329f2576e2f4f539087e0158
PerlaMoly <pengate@gmail.com>
2021-10-21 21:31:12 -0300

    quitarr menu roto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursoComponent } from './curso/curso.component';
-import { MenuComponent } from './components/cursos/menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,CursosComponent, CursoComponent, MenuComponent
+    AppComponent,CursosComponent, CursoComponent, 
   ],
   imports: [
     BrowserModule
